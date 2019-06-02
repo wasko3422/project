14461b513be2e0fc0e65b11eab808cf180fc060e
wasko3422 <panow.egor2018@gmail.com>
2019-06-02 23:16:39 +0300

    removed bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.debug = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
